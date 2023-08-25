4897b310485985b9a7ff4233d48f976d39c783ee
Kitsuya0828 <kitsuyaazuma@gmail.com>
2023-08-25 21:15:23 +0900

    feat: terraform apply succeeded

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,3 @@
-resource "random_id" "bucket_prefix" {
-  byte_length = 8
-}
-
 resource "google_service_account" "cloud_functions" {
   account_id = local.service_name
 }
@@ -30,7 +26,7 @@ resource "google_pubsub_topic" "default" {
 
 resource "google_cloud_scheduler_job" "default" {
   name     = local.service_name
-  schedule = "*/5 * * * *"
+  schedule = "*/30 * * * *"
 
   pubsub_target {
     # topic.id is the topic's full resource name.
@@ -40,7 +36,7 @@ resource "google_cloud_scheduler_job" "default" {
 }
 
 resource "google_storage_bucket" "default" {
-  name     = "${random_id.bucket_prefix.hex}-${local.service_name}" # Every bucket name must be globally unique
+  name     = local.bucket_name # Every bucket name must be globally unique
   location = "ASIA-NORTHEAST1"
 }
 
@@ -58,7 +54,8 @@ resource "google_storage_bucket_object" "default" {
 }
 
 resource "google_cloudfunctions2_function" "default" {
-  name = local.service_name
+  name     = local.service_name
+  location = local.region
 
   build_config {
     runtime     = "go120"
@@ -79,6 +76,17 @@ resource "google_cloudfunctions2_function" "default" {
     ingress_settings               = "ALLOW_INTERNAL_ONLY"
     all_traffic_on_latest_revision = true
     service_account_email          = google_service_account.cloud_functions.email
+    environment_variables = {
+      GOOGLE_CALENDAR_ID      = "XXXX"
+      NOTION_TOKEN            = "XXXX"
+      NOTION_DEFAULT_TIMEZONE = "XXXX"
+      NOTION_DATABASE_ID      = "XXXX"
+      GOOGLE_CLOUD_PROJECT_ID = "XXXX"
+      #   NOTION_DESCRIPTION_PROPERTY_NAME = "XXXX"
+      #   NOTION_TAGS_PROPERTY_NAME        = "XXXX"
+      #   NOTION_DATE_PROPERTY_NAME        = "XXXX"
+      #   NOTION_UUID_PROPERTY_NAME        = "XXXX"
+    }
   }
 
   event_trigger {
@@ -87,4 +95,18 @@ resource "google_cloudfunctions2_function" "default" {
     pubsub_topic   = google_pubsub_topic.default.id
     retry_policy   = "RETRY_POLICY_RETRY"
   }
+
+  lifecycle {
+    ignore_changes = [
+      service_config[0].environment_variables,
+      build_config[0].source[0].storage_source
+    ]
+  }
+}
+
+resource "google_firestore_database" "database" {
+  project     = data.google_project.default.project_id
+  name        = "(default)"
+  location_id = local.region
+  type        = "FIRESTORE_NATIVE"
 }
